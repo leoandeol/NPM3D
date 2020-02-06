31b1750be9237635b40da51c9f34a1975415b6c5
Léo Andeol <leo.Andeol@gmail.com>
2020-02-06 16:43:42 +0100

    poushe

--- TP4/rendering.py
+++ TP4/rendering.py
@@ -6,13 +6,29 @@ from PIL import Image
 normal = Image.open("normal.png")
 normal_np = np.array(normal)
 
-albedo = 1
-kd = 1
 
 print(normal_np.shape)
 
 class Material(object):
 
     def __init__(self):
-        self.albedo = np.array([0,255,0])
-        self.kd
+        self.albedo = 1
+        self.kd = 0.7
+        self.fd = (self.albedo+self.kd)/np.pi
+        
+        
+class LightSource(object):
+    
+    def __init__(self,x,y,z,r,g,b,it):
+        self.coord = np.array([x,y,z])
+        self.rgb = np.array([r,g,b])
+        self.int = it
+        
+def shade(normalImage, materials, lightSources):
+    for lightSource in lightSources:
+        for material in materials:
+            
+        
+if __name__ == "__main__":
+    mat = Material()
+    ls= LightSource(0,1,1,0,255,0,1)
