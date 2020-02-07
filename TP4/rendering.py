e9c5932abfba0183c12cdd3ffef37512a9781372
Lothair <lothair.kizardjian@laposte.net>
2020-02-07 15:53:42 +0100

    code modifs

--- TP4/rendering.py
+++ TP4/rendering.py
@@ -7,38 +7,51 @@ normal = Image.open("normal.png")
 normal_np = np.array(normal)
 normal_np = normal_np[:,:,:3]
 normal_np = normal_np/255
+print(np.max(normal_np))
 
 class Material(object):
 
-    def __init__(self,albedo,kd,spec,shin,wi,wo):
+    def __init__(self,albedo,kd,spec,shin):
         self.albedo = albedo
         self.kd = kd
         self.specular = spec
         self.shininess = shin
 
+    def get_lambert(self):
+        return ((self.kd * self.albedo) / np.pi)[None,:]
+
+    def get_blinn_phong(self,normal,wi,wo):
+        wh = (wi+wo)/np.linalg.norm(wi+wo)
+        wh = wh[None,:]
+        flatImage = normal.reshape((-1,3))
+        brdf = self.specular * ((flatImage @ wh.T) ** self.shininess)
+        return brdf
+
 class LightSource(object):
     
-    def __init__(self,wi,r,g,b,it):
-        self.coord = wi
-        self.rgb = np.array([r,g,b])
-        self.int = it
+    def __init__(self,coord,rgb,it):
+        self.coord = coord
+        self.rgb = rgb
+        self.it = it
+
+    def get_li(self):
+        return self.rgb * self.it
         
-def shade(normalImage, brdf, lightSource):
-    flatImage = normalImage.reshape((-1,3))
-    li = lightSource.rgb[None,:] * lightSource.int
-    image = li * brdf * (flatImage @ lightSource.coord[:,None])
-    image = image.reshape(normalImage.shape)
+def shade(normals, material, brdf_s, lightSources, wo):
+    flatImage = normals.reshape((-1,3))
+    image = np.zeros(flatImage.shape);
+    for source in lightSources:
+        if brdf_s == "lambert":
+            brdf = material.get_lambert()
+        elif brdf_s == "blinn_phong":
+            brdf = material.get_blinn_phong(normals, source.coord, wo)
+        else :
+            brdf = material.get_lambert() + material.get_blinn_phong(normals, source.coord, wo)
+        li = source.get_li()[None,:]
+        image += (li * brdf * (flatImage @ source.coord[:,None])).clip(0,1)
+    #image /= len(lightSources)
+    image = image.reshape(normals.shape)
     return image # skrattar du forlorar du
-
-def lambert(kd,albedo):
-    return ((kd*albedo)/np.pi)[None,:]
-
-def blinn_phong(material,normal,wi,wo):
-    wh = (wi+wo)/np.linalg.norm(wi+wo)
-    wh = wh[None,:]
-    flatImage = normal.reshape((-1,3))
-    brdf = material.specular * (( flatImage @ wh.T ) ** material.shininess)
-    return brdf
         
 if __name__ == "__main__":
     albedo = np.array([.75,0.9,0.6])
@@ -46,35 +59,12 @@ if __name__ == "__main__":
     spec = 1
     shin = 1
     
-    x = -10
-    y = 0
-    z = 0
-    
-    r = 1.
-    g = 1.
-    b = 1.
+    mat = Material(albedo,kd,spec,shin)
+    sources = [
+        LightSource(np.array([1,0.5,-1]),np.array([1.,0.,0.]),1.),
+        LightSource(np.array([-1,3,1]),np.array([0.,0.,1.]),1.)
+    ]
 
-    it = 1.
-
-    wi = np.array([x,y,z])
-    wo = np.array([0,0,0])    
-    
-    mat = Material(albedo,kd,spec,shin,wi,wo)    
-    ls = LightSource(wi,r,g,b,it)
-    ls2 = LightSource(np.array([0,-1,0]),r,g,b,it)
-
-    lamb = lambert(mat.kd,mat.albedo)
-    blinn = blinn_phong(mat,normal_np,ls.coord,wo)
-    brdf = lamb * blinn
-
-    image = shade(normal_np,brdf,ls)/2
-
-    lamb = lambert(mat.kd,mat.albedo)
-    blinn = blinn_phong(mat,normal_np,ls2.coord,wo)
-    brdf = lamb * blinn
-    
-    image += shade(normal_np,brdf,ls2)/2
-
-    print(np.max(image))
+    image = shade(normal_np, mat, "lambert", sources, [0,0,0])
     plt.imshow(image)
     plt.show()
